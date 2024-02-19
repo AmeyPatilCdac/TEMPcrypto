12bfa897ad90189e7800c7fe3d3b87b4dbff2d8b
AmeyPatilCdac <114463301+AmeyPatilCdac@users.noreply.github.com>
2024-02-19 14:02:45 +0530

    trial

--- IOCLconnector/IOCLconnector/src/in/cdac/java/auth/bio/Data.java
+++ IOCLconnector/IOCLconnector/src/in/cdac/java/auth/bio/Data.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlValue;
 
 //@XmlAccessorType(XmlAccessType.FIELD)
-@XmlAccessorType(XmlAccessType.FIELD)
+@XmlAccessorType(XmlAccessType.NONE)
 @XmlType(
    name = "Data",
    propOrder = {"content", "type"}
