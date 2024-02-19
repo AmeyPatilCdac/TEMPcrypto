cf0333b0b7eb40ff95c80d7fce70ebcb6039d90c
AmeyPatilCdac <114463301+AmeyPatilCdac@users.noreply.github.com>
2024-02-19 12:36:25 +0530

    XmlAccessType None to Field

--- IOCLconnector/IOCLconnector/src/in/cdac/java/auth/bio/Data.java
+++ IOCLconnector/IOCLconnector/src/in/cdac/java/auth/bio/Data.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlValue;
 
 //@XmlAccessorType(XmlAccessType.FIELD)
-@XmlAccessorType(XmlAccessType.NONE)
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(
    name = "Data",
    propOrder = {"content", "type"}
